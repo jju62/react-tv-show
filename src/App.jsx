fa9b754b63c79cb8e8893205887f8e90f2883d59
Julien Kuoch <kuochjulien@gmail.com>
2024-07-24 10:22:10 +0200

    vscode commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { TVShowAPI } from "./api/tv-show";
+import s from "./style.module.css";
+import "./global.css";
+import { useEffect, useState } from "react";
+import { BACKDROP_BASE_URL } from "./config";
+import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+import { Logo } from "./components/Logo/Logo";
+import logo from "./assets/images/logo.png";
+
+export function App() {
+  const [currentTVShow, setCurrentTVShow] = useState();
+
+  async function fetchPopulars() {
+    const populars = await TVShowAPI.fetchPopulars();
+    if (populars.length > 0) {
+      setCurrentTVShow(populars[0]);
+    }
+  }
+
+  // test
+
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+
+  console.log("***", currentTVShow);
+
+  return (
+    <div
+      className={s.main_container}
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}
+    >
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <Logo
+              image={logo}
+              title="Watowatch"
+              subtitle="Find a show you may like"
+            />
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={s.tv_show_details}>
+        {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
+      </div>
+      <div className={s.recommended_shows}>Recommended tv shows</div>
+    </div>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,4 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=ef9b684e0c34eda24cd0606e6a73d0be";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL };
